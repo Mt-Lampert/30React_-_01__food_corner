a4695e3f4e07a5b27317dec2513ea85f8fee02c4
Mt-Lampert <perrin.blog@web.de>
2022-11-27 19:12:03 +0100

    w: formatting Navbar with Bulma

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,56 @@
-import React from 'react';
-import logo from "../assets/logo.png"
+import React from "react";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar(props) {
-  return ( 
-    <div className='navbar'>
-      <div className="leftSide" data-testid="left-side">
-        <img src={logo} alt="The Logo" />
+  const links = ["Menu", "Home", "Contact", "About"];
+
+  
+  function burgerHandler() {
+    const navbarMenu = document.getElementById('navbarMenu')
+    navbarMenu.classList.toggle("is-active")
+  }
+
+  return (
+    <nav
+      className="navbar is-dark"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <figure className="image is-48x48">
+          <img src={logo} width="48" alt="The Logo" />
+        </figure>
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbar-burger"
+          onClick={burgerHandler}
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-      <div className="rightSide" data-testid="right-side">
-        This is Navbar's right side
+
+      <div id="navbarMenu" className="navbar-menu" >
+        <div className="navbar-end">
+          {links.map((link) => {
+            return (
+              <div className="navbar-item" key={link}>
+                <Link to="#">{link}</Link>
+              </div>
+            );
+          })}
+          <a href="#/" className="navbar-item">
+            {}
+          </a>
+        </div>
       </div>
-    </div> 
+    </nav>
   );
 }
 
